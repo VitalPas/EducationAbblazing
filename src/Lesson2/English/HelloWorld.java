5e7d35fb6ebec9f64e3288c4f5008356984314d1
Vital <vitalpasichnik@mai.ru>
2023-02-06 18:52:51 +0700

    init commit

---
+++ src/Lesson2/English/HelloWorld.java
@@ -0,0 +1,7 @@
+package Lesson2.English;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("HELLO!");
+    }
+}
---
+++ src/Lesson2/russian/HelloWorld.java
@@ -0,0 +1,9 @@
+package Lesson2.russian;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("HELLO!");
+    }
+
+}
+
